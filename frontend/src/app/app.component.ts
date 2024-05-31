e12b6ca5a0ad77182a86a7257a79399ec30da3f7
Julien Bastin <julienbastin96@gmail.com>
2024-05-31 23:27:32 +0200

    home page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MenuComponent} from "./menu/menu.component";
+import {HomePageComponent} from "./features/home-page/home-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
